51765b8971055b91d22ba0267f07e5f761e6035d
gith496 <gith9232@gmail.com>
2024-07-31 10:24:51 +0000

    Update config.js

--- config.js
+++ config.js
@@ -7,11 +7,11 @@ global.link = 'https://whatsapp.com/channel/0029VaWJMi3GehEE9e1YsI1S'
 global.linkGroup = 'https://chat.whatsapp.com/E3PWxdvLc7ZCp1ExOCkEGp'
 
 global.apikey = 'bf2d2cf29b3edc604b447983';
-global.ownername = "𝑆𝛪𝐷 𝐵𝛨𝛥𝛪";
-global.owner = ['917050906659'];
-global.premium = ['917050906659'];
+global.ownername = "🅼︎🅲︎🅱︎🅴︎🅽︎-🅼︎🅳︎";
+global.owner = ['233591856630'];
+global.premium = ['233591856630'];
 global.packname = "𝚜𝚝𝚒𝚌𝚔𝚎𝚛 𝚋𝚢";
-global.author = "𝐆𝐒𝐒 𝚩𝚯𝚻𝐖𝚫";
+global.author = "Ⓜ︎Ⓒ︎🅑︎Ⓔ︎🅝︎";
 global.sessionName = 'session';
 global.prefa = [".", "!", "/", "?"];
 global.typemenu = "v1"
@@ -19,13 +19,13 @@ global.menuType = "2"
 global.autoswview = true
 global.autoread = false
 global.available = false
-global.autoTyping = false
+global.autoTyping = true 
 global.autoRecord = false
 global.autoreact = false
 global.onlygroup = false
 global.onlypc = false
 global.welcome = true
-global.autoBlock = true
+global.autoBlock = false 
  
 /*REPLY MESSAGE*/
 global.mess = {
